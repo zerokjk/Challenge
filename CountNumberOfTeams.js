cbe5c675be146e705279b37f4ddcb4473f6c0d70
Tam Tran <tranchitam.um@gmail.com>
2020-07-25 02:16:26 +0700

    Added Solution

---
+++ CountNumberOfTeams.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    main();
+});
+
+function count(rating) {
+    if (rating.length < 3) {
+        return count;
+    }
+
+    const len = rating.length;
+    let dpi = new Array(len).fill(0);
+    let dpd = new Array(len).fill(0);
+    let count = 0;
+
+    for(let i = 0; i < len; i++){
+        for(let j = i; j >= 0; j--){
+            if(rating[i] > rating[j]){
+                dpi[i]++;
+                count = count + dpi[j];
+            }
+
+            if(rating[i] < rating[j]){
+                dpd[i]++;
+                count = count + dpd[j];
+            }
+        }
+    }
+
+    return count;
+}
+
+/**
+ * Sample Input: 2 5 3 4 1
+ * Sample Output: 3
+ * Testing environment: JavaScript (Node.js)
+ */
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const rating = inputString.split(' ').map(num => parseInt(num, 10));
+    const result = count(rating);
+
+    ws.write(result + "\n");
+    ws.end();
+}
